3d2e67a528e21e2da729a1ccc54e2b50f6a21fb4
pierre chene <pierre.chene44@gmail.com>
2019-02-01 16:26:57 +0100

    Init project

---
+++ src/main/java/com/java/nice/nice/NiceApplication.java
@@ -0,0 +1,14 @@
+package com.java.nice.nice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/java/nice/nice/NiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.java.nice.nice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
